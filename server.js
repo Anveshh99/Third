5c5ca0823b7ef0e86ccd3e03720671d81b0f550b
vaishnaviingole <vaishnavingole54@gmail.com>
2023-01-02 00:02:51 +0530

    done

---
+++ server.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const app=express();
+
+app.get("/",(req,resp)=>{
+    resp.send("<h1>Welcome to Fabrikam</h1>");
+})
+
+// app.get("/home",(req,resp)=>{
+//     resp.send("<h1>HALLA MADRID</h1>");
+// })
+
+const path=require("path");
+const bodyparser=require("body-parser");
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/login",(req,resp)=>{
+    resp.sendFile(__dirname+"/public/index.html")
+})
+
+app.listen(8000,()=>{
+    console.log("server running on port no 8000");
+})
